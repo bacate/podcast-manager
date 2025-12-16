cdc4b1ea6bb061c1c79fb430f53881408371ab15
bacate <igabriel.oliveira@gmail.com>
2025-12-16 12:10:58 -0300

    first commit

---
+++ docs/app.md
@@ -0,0 +1,12 @@
+# Podcast Manager
+### Descricao
+Criar um app tipo netflix para gerenciar podcast`s de todos os tipos
+
+### Dominio
+Gerenciador de podcast`s
+
+
+### Recursos
+- Listar todos os episódios de podcast`s por categorias
+    - [saúde, beleza, esporte, economia]
+- Filtrar episódios por nome
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import * as http from "http"
+import { getListEpisodes } from "./controller/podcast-controller"
+
+const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.method === "GET") {
+        await getListEpisodes(req, res)
+    }
+})
+
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`servidor iniciado na porta ${port}`)
+}) 
